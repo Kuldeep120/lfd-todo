688bc10b6429751a54f42b0fa5fc5a51e6d483f8
kuldeep <kuldeepkhatri03@gmail.com>
2021-06-11 00:02:25 +0500

    remove unnecssary feild from models

--- todo/models.py
+++ todo/models.py
@@ -8,4 +8,3 @@ class Todo(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    description2 = Column(String, index=True)
