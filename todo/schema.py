f322c9804da30e44e381764f1c5a46668461a3fd
kuldeep <kuldeepkhatri03@gmail.com>
2021-06-11 00:03:19 +0500

    added another pydantic model in schema to show data

--- todo/schema.py
+++ todo/schema.py
@@ -4,3 +4,9 @@ from typing import Optional
 class ToDo(BaseModel):
     title: str
     description: str
+
+class ShowTodo(BaseModel):
+    # title: str
+    # description: str
+    class Config():
+        orm_mode = True
