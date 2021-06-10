0176ac4d12038f6e39a06ff5ab884ec6b1b790e4
Kuldeep Khatri <kuldeep.k@tekhqs.com>
2021-06-10 06:58:51 +0500

    created different folder for todo app. and implement database connection with sqlalchemy

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from .database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+
+class Todo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    description2 = Column(String, index=True)
